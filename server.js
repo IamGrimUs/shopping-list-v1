cb2aa9e252262c6b30aa1a8e3f68db9bc41d38c0
RussellThomas <rthomas@shawscott.com>
2018-12-28 14:41:08 -0800

    CRUD set up with express.Router

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
-
 const express = require('express');
-// we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
-// parse JSON data sent in requests to this app
-const bodyParser = require('body-parser');
 
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
-const {ShoppingList} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+
+const shoppingListRouter = require('./shoppingListRouter');
+const recipesRouter = require('./recipesRouter');
 
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
-// normally you wouldn't do this. Usually your
-// server will simply expose the state of the
-// underlying database.
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
-
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+app.get('', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+// when requests come into `/shopping-list` or
+// `/recipes`, we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/shopping-list', shoppingListRouter);
+app.use('/recipes', recipesRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
